c3fa43eea29bd75a6711752b08d9f08751c43213
luke-nukem903 <lukeboren@gmail.com>
2023-01-19 17:40:26 -0600

    began building HTML CSS and JavaScript

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        question: "In what year was the first programmable computer created?\n(A) 1936\n(B) 1993\n(C) 1974\n(D) All of the Above"
+        answer: 'A'
+    },
+    {
+        question: "What is a DOM?\n(A) That guy from Fast and Furious.\n(B) Dome spelled incorrectly.\n(C) A pizza restaurant.\n(D) None of the Above."
+        answer: 'D'
+    },
+    {
+        question: "What item does this console command: '000139B9' spawn in Skyrim?\n(A) A Dwarven Centurion.\n(B) The Greybeards\n(C) A Daedric Sword\n(D) A Sweet Roll"
+        answer: 'C'
+    },
+    {
+        question: "What was the first webpage named?\n(A) http://www.google.com \n(B) http://info.cern.ch \n(C) http://youtube.com \n(D) http://nic.nikhef.nl"
+        answer: 'B'
+    },
+    {
+        question: "What is the purpose of JavaScript?\n(A) Make designs in the foam on cappuccino\n(B) Create headaches for junior coders.\n(C) Create dynamic and interactive web content like applications and browsers.\n(D) Script for screen readers."
+        answer: 'C'
+    },
+    {
+        question: "What are API's?\n(A) Application Programming Interface.\n(B) Aplication Programming Interface.\n(C) Application Programing Interface.\n(D) Application Programming Enterface."
+        answer: 'A'
+    }
+]
